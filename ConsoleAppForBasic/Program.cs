e64dd30fdd145f478856a4570ce89e51ef6ce368
Gino <ggino3567@gmail.com>
2025-07-11 02:18:05 +0800

    Docs: Add ClassLibrary - add classlibrary file with custom namespace and function for practice [2025/7/10]

--- ConsoleAppForBasic/Program.cs
+++ ConsoleAppForBasic/Program.cs
@@ -1,6 +1,9 @@
-﻿namespace ConsoleAppForBasic
+﻿//Debug need Find Root Cause!
+using Tools;
+
+namespace ConsoleAppForBasic
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -9,41 +12,41 @@
              - 通常都用下列兩種較多
              */
 
-            // 像是JS中的Array => 查詢、存放一堆資料
-            List<string> OrderList = new List<string>{ "哈利波特", "魔戒", "沉默之丘", "惡靈古堡" };
+            //像是JS中的Array => 查詢、存放一堆資料
+            List<string> OrderList = new List<string>{ "List1", "List2", "List3", "List4" };
 
-            // 請注意! foreach 是唯讀操作，你不能在裡面「改動集合本身」
-            // 要修改內容只能使用for迴圈+索引
-            // 看看foreach在List長怎樣
+            //請注意! foreach 是唯讀操作，你不能在裡面「改動集合本身」
+            //要修改內容只能使用for迴圈+索引
+            //看看foreach在List長怎樣
             foreach (var item in OrderList)
             {
                 Console.WriteLine($"List Foreach => {item}");
             }
 
-            // 像是JS中的Map => 快速查 key 對應值（像狀態轉換、選單列表）
+            //像是JS中的Map => 快速查 key 對應值（像狀態轉換、選單列表）
             Dictionary<int, string> MemberList = new Dictionary<int, string>
             {
-                {0, "陳奕迅" },
-                {1, "周杰倫" },
-                {2, "范逸臣" },
+                {0, "Dictionary1" },
+                {1, "Dictionary2" },
+                {2, "Dictionary3" },
             };
             
-            // foreach in Dictionary
+            //foreach in Dictionary
             foreach (var Checker in MemberList)
             {
                 Console.WriteLine($"Dictionary Foreach => {Checker.Key + 1}\n{Checker.Value}");
             }
 
-            // 宣告MemberProfile<string> - MemberList[0 => "陳奕迅"]
+            //宣告MemberProfile<string> - MemberList[0 => "陳奕迅"]
             string MemberProfile = MemberList[0];
 
-            // 印出樣板文字
+            //印出樣板文字
             //Console.WriteLine($"第一位成員: {MemberProfile}");
 
-            // 下列大概了解即可
-            Queue<string> Queue = new Queue<string>(); // 先進先出（排隊系統）
-            Stack<string> Stack = new Stack<string>(); // 後進先出（像瀏覽器歷史紀錄）
-            HashSet<string> HashSet = new HashSet<string>(); // 排除重複數值
+            //下列大概了解即可
+            Queue<string> Queue = new Queue<string>(); //先進先出（排隊系統）
+            Stack<string> Stack = new Stack<string>(); //後進先出（像瀏覽器歷史紀錄）
+            HashSet<string> HashSet = new HashSet<string>(); //排除重複數值
 
             /*
             for (int i = 0; i < OrderList.Count; i++)
@@ -51,6 +54,16 @@
                 Console.WriteLine($"第{i + 1}部電影: {OrderList[i]}");
             }
              */
+
+            double GetSum = Calculator.Add(1.5, 2.0);
+            Console.WriteLine($"Add: {GetSum}");
+            GetSum = Calculator.Sub(1.5, 2.0);
+            Console.WriteLine($"Sub: {GetSum}");
+            GetSum = Calculator.Mul(1.5, 2.0);
+            Console.WriteLine($"Mul: {GetSum}");
+            GetSum = Calculator.Div(1.5, 0);
+            Console.WriteLine($"Div: {GetSum}");
         }
     }
 }
+
---
+++ MySolution/Calculator.cs
@@ -0,0 +1,33 @@
+﻿namespace Tools
+{
+    public class Calculator
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Mul(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Div(double x, double y)
+        {
+            if (y < 0)
+            {
+                double ErrorMessage = double.PositiveInfinity;
+                return ErrorMessage;
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+    }
+}
